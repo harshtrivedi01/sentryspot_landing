22e0d276af663ce204227c4c08e4160cf41e5f90
Nandini <nandinigupta63696@gmail.com>
2024-06-13 18:15:37 +0530

    added subscription

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from'./Component/Footer/Footer'
 import Home_first from './Component/Home/Home_first'
 import Job_Search from './Component/Home/Job_Search'
 import Resume_Guide from './Component/Home/Resume_Guide'
+import Subscription from './Component/Home/Subscription'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
    
     <Home_first/>
     <Job_Search/>
+    <Subscription/>
     {/* <Resume_Guide/> */}
      {/* <FAQ/>  */}
      
--- src/Component/Home/subscription.jsx
+++ src/Component/Home/subscription.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
+import './Home.css'
 
 function subscription() {
   return (
     <>
 
      <div>
-        <div>
-            <div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+      <div>
+        <h1 className=' text-center font-bold text-3xl py-5'>Want To Supercharge Your Career Growth With Our AI Tool?</h1>
+        <div className='  mx-auto md:w-[600px] py-3'>Get started at no cost to tap into Abroadium's premium features and power up your job search. Experience our amazing AI enabled easy to use dashboard.</div>
+        <div className=' px-2 flex justify-center'>
+          
+            <div className=' border-t-4 border-t-gray-900 shadow-xl shadow-gray-700 rounded-lg px-3 py-3'>
+              <h1 className=' font-bold text-xl'>Abroadium's AI Resume</h1>
+                <div className=' flex gap-2 items-center font-medium'><i className="fa-solid fa-check border-2 px-2 py-2 rounded-full text-white bg-green-500 "></i>AI Score</div>
+                <div className=' flex gap-2 items-center font-medium'><i className="fa-solid fa-check border-2 px-2 py-2 rounded-full text-white bg-green-500"></i>AI Parsing</div>
+                <div className=' flex gap-2 items-center font-medium'><i className="fa-solid fa-check border-2 px-2 py-2 rounded-full text-white bg-green-500"></i>Resume Building</div>
+                <div className=' flex gap-2 items-center font-medium'><i className="fa-solid fa-check border-2 px-2 py-2 rounded-full text-white bg-green-500"></i>AI Enhancer</div>
+                <div className=' flex gap-2 items-center font-medium'><i className="fa-solid fa-check border-2 px-2 py-2 rounded-full text-white bg-green-500"></i>Match & Apply</div>
+                <div className=' flex gap-2 items-center font-medium'><i className="fa-solid fa-check border-2 px-2 py-2 rounded-full text-white bg-green-500"></i>AI Skill Test</div>
+                <div className=' flex gap-2 items-center font-medium'><i className="fa-solid fa-check border-2 px-2 py-2 rounded-full text-white bg-green-500"></i>Skill Badges</div>
+                <div className=' flex gap-2 items-center font-medium'><i className="fa-solid fa-check border-2 px-2 py-2 rounded-full text-white bg-green-500"></i>& More</div>
             </div>
+            
+        </div>
+        <div className=' flex justify-center mt-6 mb-3'>
+        <button className=' px-3 py-3 rounded-2xl font-semibold text-white shadow-xl shadow-slate-800' id="home_third">Get Started For Free</button>
+        </div>
+        
         </div>
         </div> 
     </>
