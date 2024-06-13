ea299928ab4f478336b016db7477093f1b666b5e
Nandini <nandinigupta63696@gmail.com>
2024-06-13 18:22:05 +0530

    added pay

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from'./Component/Footer/Footer'
 import Home_first from './Component/Home/Home_first'
 import Job_Search from './Component/Home/Job_Search'
 import Resume_Guide from './Component/Home/Resume_Guide'
-import Subscription from './Component/Home/Subscription'
+import Subscription from './Component/Home/Pay'
 
 function App() {
 
